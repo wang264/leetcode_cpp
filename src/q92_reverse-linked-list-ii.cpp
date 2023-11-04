4c331c1102a36ae6554de4acb85ee0fec0c8fd20
Andy <wang264@hotmail.com>
2023-11-04 09:59:42 -0400

    Update q92_reverse-linked-list-ii.cpp

--- src/q92_reverse-linked-list-ii.cpp
+++ src/q92_reverse-linked-list-ii.cpp
@@ -1,14 +1,53 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
+//Definition for singly-linked list.
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+// this is a better solution.
 class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        // empty list
+        if (!head) return nullptr;
+
+        //move the two pointer untill they reach the proper starting point in the list.
+        ListNode* curr = head;
+        ListNode* prev = nullptr;
+
+        for (int i=0; i<left-1; i++) {
+            prev = curr;
+            curr = curr->next;
+        }
+
+        // the two pointers that will fix the final connections.
+        ListNode* new_tail = curr; 
+        ListNode* con = prev;
+
+        // Iteratively reverse the nodes util n become 0
+        for (int i=left; i<=right;i++)  {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        // adjust the final connections
+        if (con) {
+            con->next = prev;   //prev is the tail of the sublist before reversal.
+        } else {
+            head = prev;
+        }
+        new_tail->next = curr; // curr is one over the node the #right-th pointer.
+        return head; 
+        
+    }
+};
+
+class Solution2 {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
         ListNode* l = nullptr;
